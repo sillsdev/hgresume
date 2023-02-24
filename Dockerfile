01d1edafc07b8f43476422f16cec0161db28f0da
Kevin Hahn <hahn.kev@gmail.com>
2023-02-24 14:42:32 +0700

    fix error if time isn't installed. Add github ci to build image.

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
-FROM php:7.4-apache
+FROM php:8-apache
 EXPOSE 80
 # Use the default production configuration
 RUN mv "$PHP_INI_DIR/php.ini-production" "$PHP_INI_DIR/php.ini"
 #todo use chorus version instead
-RUN apt-get update && apt-get install -y mercurial
+RUN apt-get update && apt-get install -y mercurial time
 RUN a2enmod rewrite
+RUN mkdir -p /var/cache/hgresume/ /var/vcs/ && chown -R www-data:www-data /var/cache/hgresume/ /var/vcs/
 COPY --chown=www-data api /var/www/html/api/v03/
--- api/index.php
+++ api/index.php
@@ -7,6 +7,11 @@ require_once('src/HgResumeApi.php');
 //$repoPath = sys_get_temp_dir() . "/hgresume_repoTestEnvironment";
 $api = new HgResumeAPI($repoSearchPaths);
 
+// in docker time might not be installed and it's used by
+// AsyncRunner, just check at the start because throwing in AsyncRunner doesn't help much
+if (!file_exists('/usr/bin/time')) {
+    throw new Exception("/usr/bin/time, required by AsyncRunner");
+}
 // second param is debug mode
 $restServer = new RestServer($api, false);
 
